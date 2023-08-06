474e156ab035470170ddf244dd39bcc8e313c0a0
ashishranjan <02.02as.ra@gmail.com>
2023-08-06 21:23:09 +0530

    Project done debugging left

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import {addUser,getUsers} from "../controller/addUserController.js";
 import { addConvo,getConvo } from "../controller/addConvoController.js";
 import { saveMessage,getMessage } from "../controller/messageController.js";
-import { uploadFile,getImage } from "../controller/image-controller.js";
+// import { uploadFile,getImage } from "../controller/image-controller.js";
 import upload from "../utils/upload.js";
 
 const routes = express.Router();
@@ -15,8 +15,8 @@ routes.post("/message/add",saveMessage);
 routes.get("/message/get/:id",getMessage);
 
 //route for uploading and downloading files
-routes.post("/file/upload",upload.single("file"),uploadFile);
-route.get("/file/:filename",getImage);
+// routes.post("/file/upload",upload.single("file"),uploadFile);
+// routes.get("/file/:filename",getImage);
 
 
 export default routes;
